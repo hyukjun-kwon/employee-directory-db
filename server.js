3d84b14d7531f70b92f2bf829268b266c7a461ce
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-03-20 22:40:02 -0700

    CORS - allow METHODS: PUT, GET, POST, DELETE, OPTIONS

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 // CORS
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
