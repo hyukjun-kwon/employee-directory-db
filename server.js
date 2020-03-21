89a48197113dd924e9ac34ecc06f624e5b932481
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-03-20 22:19:21 -0700

    CORS

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
